44109baabef4f5570f352ab85db25fdf92ace891
Amoo Hesam <hesamgholami@yahoo.com>
2019-10-08 18:43:27 +0330

    * Update plugin name

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: افزونه پرداخت جیبی‌مو برای وردپرس
+Plugin Name: WooCommerce Jibimo Payment Gateway
 Version: 1.0
 Description:  با استفاده از این افزونه می‌توانید به راحتی به درگاه پرداخت جیبی‌مو متصل شوید.
 Plugin URI: https://jibimo.com
